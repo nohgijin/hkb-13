8ade1d4bcaf405b5c0f601f51898394df70636a0
nohgijin <ynda11111@naver.com>
2020-07-27 16:39:01 +0900

    config: app.js sendFile 수정

--- src/app.js
+++ src/app.js
@@ -10,8 +10,7 @@ const app = express()
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
-app.use('/dist', express.static(appRoot + '/public'))
+app.use(express.static(__dirname + '/../dist'))
 
 app.get('/', (req, res) => {
   res.sendFile(appRoot.resolve('/dist/index.html'))
