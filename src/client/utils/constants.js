586a655f230c7e73abd0a56b4ca166f0b6d3453a
dnacu <winteri1276@gmail.com>
2020-07-28 16:28:06 +0900

    chore: define categories to constant variables

---
+++ src/client/utils/constants.js
@@ -0,0 +1,18 @@
+// 수입 카테고리 종류: 월급, 용돈, 기타수입
+// 지출 카테고리 종류: 식비, 생활, 쇼핑/뷰티, 교통, 의료/건강, 문화/여가, 미분류
+
+export const INCOME_CATEGORY = {
+  SALARY: '월급',
+  POCKET_MONEY: '용돈',
+  OTHERS: '기타수입',
+}
+
+export const EXPENSE_CATEGORY = {
+  FOOD: '식비',
+  LIFE: '생활',
+  SHOP_BEAUTY: '쇼핑/뷰티',
+  TRAFFIC: '교통',
+  MEDICAL_HEALTH: '의료/건강',
+  CULTURE: '문화/여가',
+  UNDEF: '미분류',
+}
