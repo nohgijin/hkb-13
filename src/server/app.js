85161215c88e519177414a9f9529f206cdb7973f
dnacu <winteri1276@gmail.com>
2020-07-30 10:25:21 +0900

    chore: change static path

--- src/server/app.js
+++ src/server/app.js
@@ -11,7 +11,7 @@ const { reportsListRouter } = require('./router')
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static(__dirname + '/../dist'))
+app.use(express.static(__dirname + '/../../dist'))
 
 // app.get('/', (req, res) => {
 // res.sendFile(appRoot.resolve('/dist/index.html'))
