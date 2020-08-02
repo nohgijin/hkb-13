c10d3847f2aa88004244d8cd80e39e916cdcc473
dnacu <winteri1276@gmail.com>
2020-08-02 13:49:13 +0900

    refactor: reportsList 리팩토링 작업
    - 팀원과의 통일성을 위해 custom element방식 제거
    - report 생성하는 부분을 모듈화함
    - $root라는 변수를 클래스의 최상위 element처럼 두고 사용하고자 하였음

--- src/client/scripts/reportsList/reportsList.js
+++ src/client/scripts/reportsList/reportsList.js
@@ -1,28 +1,13 @@
-import { getReportsListAPI } from '@/client/apis/getReportsListAPI'
+import { reportElm } from './report'
+import { hkbModel } from '@/client/models/hkbModel'
 
 import { generateElement } from '@/client/utils/htmlGenerator'
 
-import { INCOME_CATEGORY, EXPENSE_CATEGORY } from '@/client/utils/constants'
-
-export class ReportsList extends HTMLElement {
+export class ReportsList {
   constructor() {
-    super()
-    this.initElements()
-  }
+    this.$root = generateElement(`<main class="reports-page"></main>`)
 
-  connectedCallback() {
-    this.initVariables()
-    this.initEvents()
-    this.getReportsList()
-  }
-
-  initEvents() {}
-
-  initVariables() {
-    this.year = this.getAttribute('data-year')
-    this.month = this.getAttribute('data-month')
-
-    this.listSectionElm = this.querySelector('section.list-section')
+    hkbModel.subscribe(this.render.bind(this))
   }
 
   getDateString(year, month, date) {
@@ -32,58 +17,19 @@ export class ReportsList extends HTMLElement {
     }`
   }
 
-  parseToLocalMoneyString(money) {
-    return String(money).replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,')
-  }
-
-  getNameByCategory(category) {
-    return category === INCOME_CATEGORY.SALARY
-      ? ['money', 'money_dollar']
-      : category === INCOME_CATEGORY.POCKET_MONEY
-      ? ['money', 'money_dollar']
-      : category === INCOME_CATEGORY.OTHERS
-      ? ['money', 'money_dollar']
-      : category === EXPENSE_CATEGORY.FOOD
-      ? ['food', 'tray']
-      : category === EXPENSE_CATEGORY.LIFE
-      ? ['food', 'tray']
-      : category === EXPENSE_CATEGORY.SHOP_BEAUTY
-      ? ['shop', 'cart']
-      : category === EXPENSE_CATEGORY.TRAFFIC
-      ? ['transport', 'car_fill']
-      : category === EXPENSE_CATEGORY.MEDICAL_HEALTH
-      ? ['shop', 'cart']
-      : category === EXPENSE_CATEGORY.CULTURE
-      ? ['culture', 'film']
-      : ['shop', 'cart']
-  }
-
-  async getReportsList() {
-    const { year, month } = this
-    const reportsList = await getReportsListAPI({ year, month })
-
+  async render({ year, month, reportsList }) {
     if (!reportsList) return
 
-    const fragmentElm = document.createDocumentFragment()
+    const listElm = generateElement(`<section class="list-section"></section>`)
 
     let prevDate = null
     let prevReportElm = null
     reportsList.forEach((report) => {
-      const { category, content, date, id, paymentMethod, price, type } = report
-      const sign = type === 'income' ? 1 : -1
-      const money = this.parseToLocalMoneyString(price * sign)
-      const [className, iconName] = this.getNameByCategory(category)
+      const { date } = report
 
       if (prevDate === date && prevReportElm) {
         const reportsBodyElm = prevReportElm.querySelector('.report-body')
-        const reportRowElm = generateElement(`
-          <div class="row" data-report-id=${id}>
-            <i class="icon category-icon ${className}" title="${category}">${iconName}</i>
-            <div class="description">${content}</div>
-            <div class="payment">${paymentMethod}</div>
-            <div class="price ${type}">${money}원</div>
-          </div>
-        `)
+        const reportRowElm = generateElement(reportElm(report))
 
         reportsBodyElm.appendChild(reportRowElm)
       } else {
@@ -93,33 +39,18 @@ export class ReportsList extends HTMLElement {
               <div class="day">${this.getDateString(year, month, date)}</div>
             </div>
             <div class="report-body">
-              <div class="row" data-report-id=${id}>
-              <i class="icon category-icon ${className}" title="${category}">${iconName}</i>
-                <div class="description">${content}</div>
-                <div class="payment">${paymentMethod}</div>
-                <div class="price ${type}">${money}원</div>
-              </div>
+              ${reportElm(report)}
             </div>
           </div>
         `)
 
         prevDate = date
         prevReportElm = dailyReportElm
-        fragmentElm.appendChild(dailyReportElm)
+        listElm.appendChild(dailyReportElm)
       }
     })
 
-    this.listSectionElm.innerHTML = ''
-    this.listSectionElm.appendChild(fragmentElm)
-  }
-
-  initElements() {
-    this.innerHTML = `
-      <main class="reports-page">
-        <section class="list-section"></section>
-      </main>
-    `
+    this.$root.innerHTML = ''
+    this.$root.appendChild(listElm)
   }
 }
-
-customElements.define('reports-list', ReportsList)
