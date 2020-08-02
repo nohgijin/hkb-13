c1d37b62f0b33a44be5456bf56ddf9c190cc2264
nohgijin <ynda11111@naver.com>
2020-08-03 02:39:35 +0900

    feat: Login class 작성 Login view를 제공하는 클래스 작성

---
+++ src/client/scripts/login/Login.js
@@ -0,0 +1,8 @@
+import { generateElement } from '@/client/utils/htmlGenerator'
+
+export class Login {
+  constructor() {
+    this.$root = generateElement(`<main class="login-page">
+    <a href='/login/naver'><img src="/image/naverLogin.png"></a></main>`)
+  }
+}
