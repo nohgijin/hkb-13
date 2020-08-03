6cfa49d0060f835e4519f64920fdbeb7a1a455ac
nohgijin <ynda11111@naver.com>
2020-08-04 02:10:40 +0900

    markup: login페이지 마크업

--- src/client/scripts/login/Login.js
+++ src/client/scripts/login/Login.js
@@ -4,12 +4,13 @@ export class Login {
   constructor() {
     this.$root = generateElement(`
       <main class="login-page">
+        <h1>돈을 아끼자!</h1>
         <div id="naverIdLogin"></div>
-        <a href='/login/google' class="btn btn-block btn-social btn-google">
+        <a href='/login/google' class="btn btn-block btn-social btn-lg btn-google">
           <span class="fa fa-google"></span>
           Sign in with Google
         </a>
-        <a href='/login/github' class="btn btn-block btn-social btn-github">
+        <a href='/login/github' class="btn btn-block btn-social btn-lg btn-github">
         <span class="fa fa-github"></span>
         Sign in with GitHub
       </a>
@@ -21,7 +22,7 @@ export class Login {
     var naverLogin = new naver.LoginWithNaverId({
       clientId: naverConfig.CLIENT_ID,
       callbackUrl: naverConfig.CALLBACK_URL,
-      loginButton: { color: 'green', type: 3, height: 60 },
+      loginButton: { color: 'green', type: 3, height: 52 },
     })
     naverLogin.init()
   }
