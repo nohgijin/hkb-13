1e903a0f1553f1605e4258ff1e71f2c2499c1138
nohgijin <ynda11111@naver.com>
2020-08-04 01:51:02 +0900

    feat: login페이지 버튼 이미지 google,github의 경우 bootstrap을, 네이버의 경우 naver developer을 사용

--- src/client/scripts/login/Login.js
+++ src/client/scripts/login/Login.js
@@ -1,13 +1,28 @@
 import { generateElement } from '@/client/utils/htmlGenerator'
-
+const { naverConfig } = require('../../../config/naverPassport')
 export class Login {
   constructor() {
     this.$root = generateElement(`
       <main class="login-page">
-        <a href='/login/naver'>
-        <img src="/image/naverLogin.png"></a>
-        <a href='/login/google'>구글로로그인</a>
+        <div id="naverIdLogin"></div>
+        <a href='/login/google' class="btn btn-block btn-social btn-google">
+          <span class="fa fa-google"></span>
+          Sign in with Google
+        </a>
+        <a href='/login/github' class="btn btn-block btn-social btn-github">
+        <span class="fa fa-github"></span>
+        Sign in with GitHub
+      </a>
       </main>
     `)
+  }
+
+  loadNaverBtn() {
+    var naverLogin = new naver.LoginWithNaverId({
+      clientId: naverConfig.CLIENT_ID,
+      callbackUrl: naverConfig.CALLBACK_URL,
+      loginButton: { color: 'green', type: 3, height: 60 },
+    })
+    naverLogin.init()
   }
 }
