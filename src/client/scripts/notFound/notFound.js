90f1ec5c0c6e9045d76df2e5e8b8e961f7a6209e
dnacu <winteri1276@gmail.com>
2020-08-03 15:38:42 +0900

    refactor: notFound page class component로 변경

--- src/client/scripts/notFound/notFound.js
+++ src/client/scripts/notFound/notFound.js
@@ -1,20 +1,15 @@
-export class NotFound extends HTMLElement {
-  constructor() {
-    super()
-    this.initElements()
-  }
+import { generateElement } from '@/client/utils/htmlGenerator'
 
-  initElements() {
-    this.innerHTML = `
-    <div id="not-found">
-      <div class="rain">
-        <div class="otl"><img src="https://baemin.com/img/img-title-error.png" alt="">
-          <p>404 NOT FOUND</p><a href="/reports"><img src="https://baemin.com/img/btn-home.png" alt=""></a>
+export class NotFound {
+  constructor() {
+    this.$root = generateElement(`
+      <div id="not-found">
+        <div class="rain">
+          <div class="otl"><img src="https://baemin.com/img/img-title-error.png" alt="">
+            <p>404 NOT FOUND</p><a href="/reports"><img src="https://baemin.com/img/btn-home.png" alt=""></a>
+          </div>
         </div>
       </div>
-    </div>
-    `
+    `)
   }
 }
-
-customElements.define('not-found', NotFound)
--- src/client/scripts/router.js
+++ src/client/scripts/router.js
@@ -2,9 +2,11 @@ import { Login } from './login/Login'
 import { ReportsList } from './reportsList/ReportsList'
 import { Calendar } from './calendar/Calendar'
 import { NavigationBar } from './navigation/Navigation'
-import './notFound/notFound'
+import { NotFound } from './notFound/notFound'
 
 const routePage = () => {
+  const pathname = location.pathname
+
   const app = document.querySelector('.app')
   app.innerHTML = ''
 
@@ -14,25 +16,27 @@ const routePage = () => {
     return
   }
 
-  app.append(new NavigationBar().$root)
-
   // render report page
   if (pathname === `/reports`) {
+    app.append(new NavigationBar().$root)
     app.append(new ReportsList().$root)
     return
   }
 
   // render calendar page
   else if (pathname === `/calendar`) {
+    app.append(new NavigationBar().$root)
     app.append(new Calendar().$root)
     return
   }
 
   // render statistics page
   else if (pathname === `/statistics`) {
+    app.append(new NavigationBar().$root)
     return
   } else {
     // render notFound page
+    app.append(new NotFound().$root)
   }
 }
 
