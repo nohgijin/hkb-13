de9ca8aba3c44871fccdacda1f48093da801d0fe
nohgijin <ynda11111@naver.com>
2020-08-03 14:14:09 +0900

    feat: 현재 날짜 표시 년,월,일 비교해서 같다면 날짜 색깔 바뀜

--- src/client/scripts/calendar/Day.js
+++ src/client/scripts/calendar/Day.js
@@ -2,13 +2,13 @@ import { CALENDAR_CLASS } from '../../utils/constants'
 import { comma } from '@/client/utils/comma'
 
 export default class Day {
-  constructor({ month, date, income, expense, disable }) {
+  constructor({ year, month, date, income, expense, disable }) {
+    this.year = year
     this.month = month
     this.date = date
     this.disable = disable
     this.income = income
     this.expense = expense
-
     this.init()
   }
 
@@ -16,13 +16,29 @@ export default class Day {
     this.setElements()
   }
 
+  isToday() {
+    this.today = new Date()
+    if (
+      this.today.getFullYear() == this.year &&
+      this.today.getMonth() == this.month - 1 &&
+      this.today.getDate() == this.date
+    ) {
+      return true
+    }
+    return false
+  }
+
   setElements() {
+    if (this.isToday()) this.same = true
+
     if (this.income != 0) this.income = '+' + comma(this.income)
     if (this.expense != 0) this.expense = '-' + comma(this.expense)
 
     this.template = `
     <div class='${this.disable ? 'disable' : ''}'>
-      <div class='${CALENDAR_CLASS.DATE}'>${this.date}</div>
+      <div class='${this.same ? 'today' : ''}'>
+        <div class='${CALENDAR_CLASS.DATE}'>${this.date}</div>
+      </div>
       <div class='${CALENDAR_CLASS.INCOME}'>${this.income}</div>
       <div class='${CALENDAR_CLASS.EXPENSE}'>${this.expense}</div>
     </div>
