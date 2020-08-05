68bd7dd7ff87df191ab87cf142ce492a10978aa9
dnacu <winteri1276@gmail.com>
2020-08-05 15:24:55 +0900

    fix: template string 내부 값이 0일때 출력이 안되는 문제 수정

--- src/client/scripts/statistics/barChart.js
+++ src/client/scripts/statistics/barChart.js
@@ -7,17 +7,16 @@ export const getBarChartElm = (statistics) => {
 
   statistics.forEach((data, idx) => {
     const { category, price, percent } = data
-    console.log(percent * 100)
     const rowElm = generateElement(html`<div
       class="row color-${idx.toString()}"
     >
       <div class="name">${category}</div>
-      <div class="percent">${percent * 100}%</div>
+      <div class="percent">${(percent * 100).toString()}%</div>
       <div class="chart"><div class="bar"></div></div>
       <div class="price">${parseToLocalMoneyString(price)}원</div>
     </div>`)
 
-    rowElm.querySelector('.bar').style.width = `${percent * 100}%`
+    rowElm.querySelector('.bar').style.width = `${(percent * 100).toString()}%`
 
     barChartElm.append(rowElm)
   })
