bb274458341f149a3700f13b4e1cacfddc4ca642
dnacu <winteri1276@gmail.com>
2020-08-05 22:05:38 +0900

    chore: get statistics data clien api 정의

---
+++ src/client/apis/statisticsAPI.js
@@ -0,0 +1,38 @@
+export const getCategoryStatisticsDataAPI = async (params) => {
+  const { year, month } = params
+
+  const response = await fetch(
+    `/api/board/${year}/${month}/statistic/category`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+
+  if (!response.ok) return null
+  if (response.redirected) location.href = response.url
+
+  const { categoryStatisticsData } = await response.json()
+
+  return categoryStatisticsData
+}
+
+export const getDailyStatisticsDataAPI = async (params) => {
+  const { year, month } = params
+
+  const response = await fetch(`/api/board/${year}/${month}/statistic/daily`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  if (!response.ok) return null
+  if (response.redirected) location.href = response.url
+
+  const { categoryStatisticsData } = await response.json()
+
+  return categoryStatisticsData
+}
