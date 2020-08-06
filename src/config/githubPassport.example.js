b7c4b0654d66848d3fbf3a3d44133fe411f0b40e
nohgijin <ynda11111@naver.com>
2020-08-06 18:25:07 +0900

    chore: url 수정

--- src/config/githubPassport.example.js
+++ src/config/githubPassport.example.js
@@ -1,7 +1,7 @@
 const githubConfig = {
   CLIENT_ID: '',
   CLIENT_SECRET: '',
-  CALLBACK_URL: 'http://localhost:3000/login/github/callback',
+  CALLBACK_URL: 'http://13.125.215.184:3000/login/github/callback',
 }
 
 module.exports = { githubConfig }
