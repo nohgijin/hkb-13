dda00575699c5277220152248dee764af60897ef
nohgijin <ynda11111@naver.com>
2020-08-07 05:10:21 +0900

    test

--- src/config/naverPassport.example.js
+++ src/config/naverPassport.example.js
@@ -1,7 +1,7 @@
 const naverConfig = {
   CLIENT_ID: '',
   CLIENT_SECRET: '',
-  CALLBACK_URL: 'http://localhost:3000/login/naver/callback',
+  CALLBACK_URL: 'http://ec2-13-125-215-184.ap-northeast-2.compute.amazonaws.com:3000/login/naver/callback',
 }
 
 module.exports = { naverConfig }
